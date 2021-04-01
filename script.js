8c3c9597e96f486760772345ffd589c9f9450f1b
Jairo Medeiros Silva <69281962+Jairomedeiros2512@users.noreply.github.com>
2021-03-31 23:39:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function compute()
-{
+function computer() {
+  {
     p = document.getElementById("principal").value;
-    
-}
-        
+    y = document.getElementById("years").value;
+    t = document.getElementById("rate").value;
+    t = principal * years * rate / 100;
+  }
+}        
